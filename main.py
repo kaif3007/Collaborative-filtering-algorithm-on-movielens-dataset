d4ae26aa876fcb402b11180598d1495785a1e929
Md kaif khan <40945482+kaifkhan3007@users.noreply.github.com>
2019-09-12 09:58:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#content based recommendation because we have feature matrix of users and movies.
+
+#collaborative filtering algorithm.  both user and movie collaborate to improve themselves . users help system learn its featur matrix and
+#in return system is helping user to learn its feature matrix.
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -33,7 +39,8 @@ def cost(parameters,Y,R,features,lemda):
     error=np.multiply(feature_matrix.dot(theta.T)-Y,R)
     squared_error=np.power(error,2)
     cost=(np.sum(squared_error))/2
-
+    
+    #regularisation added.
     cost+=(lemda*(np.sum(np.square(theta))))/2
     cost+=(lemda*(np.sum(np.square(feature_matrix))))/2
 
@@ -46,9 +53,12 @@ def cost(parameters,Y,R,features,lemda):
 def gradients(error,feature_matrix,theta,lemda):
     feature_matrix_grad=np.zeros(feature_matrix.shape)
     theta_grad=np.zeros(theta.shape)
-
+    
+    #regularization factor included to avoid over fitting.
     feature_matrix_grad=error.dot(theta)+lemda*feature_matrix
     theta_grad=(error.T).dot(feature_matrix)+lemda*theta
+    
+    #optimiser function needs flattened parameters.
     grads=np.concatenate((np.ravel(feature_matrix_grad),np.ravel(theta_grad)))
     return grads
 
@@ -59,12 +69,12 @@ f = open('movie_ids.txt')
 for line in f:
    tokens = line.split(' ')
    tokens[-1] = tokens[-1][:-1]
+   #movie_idx stores index of movies along with movies name.
    movie_idx[int(tokens[0]) - 1] = ' '.join(tokens[1:])
 
 ratings = np.zeros((1682, 1))
 
-#rating by a new user
-
+#rating by a new user.user rates 10 movies of his choice. he may rate any number of movies including zero.
 ratings[0] = 4
 ratings[6] = 3
 ratings[11] = 5
@@ -101,12 +111,19 @@ alpha=10
 
 features_matrix= np.random.random(size=(movies, features))
 theta = np.random.random(size=(users, features))
+
+#parameters needs to be flattened before feeding it to a minimise optiser function.
 parameters = np.concatenate((np.ravel(features_matrix), np.ravel(theta)))
 
+#new normalised matrix.
 Y_normalised=np.zeros((movies,users))
 Y_mean=np.zeros((movies,1))
 
 #mean normalisation . A technique useful for new user
+#parameter matrix of new user will become zero intitially if user hasn't rated any movie initially.
+#so for each movie prediction will be zero.  mean normalisation handles this case efficiently as to finally adds mean of movies to the users
+#to the predicted ratings.
+
 
 for i in range(movies):
     cnt=0
@@ -120,22 +137,31 @@ for i in range(movies):
            Y_normalised[i][j]=Y[i][j]-cos/cnt
     Y_mean[i]=cos/cnt
 
-#optimisation function
+#optimisation function to optimize a multivariable scalar function. 
+#func takes optimisation function.
+#x0 takes parameters to optimize.
+#args takes remaining parameters of the optimisation function apart from parameters to be optimised.
 fmin = minimize(fun=cost, x0=parameters, args=(Y_normalised, R, features,alpha), method='CG', jac=True, options={'maxiter': 100})
 
 print(fmin)
 
+#fmin returns our optimised parameters.
 X = np.matrix(np.reshape(fmin.x[:movies * features], (movies, features)))
 Theta = np.matrix(np.reshape(fmin.x[movies * features:], (users, features)))
 
 print(X.shape)
 print(theta.shape)
 
-# top 10 prediction for new user
+#our new prediction matrix after adding of new user.
 predictions = X * Theta.T
+
+#adding mean value that we subtracted earlier during mean normalisation.
 my_preds = predictions[:, -1] + Y_mean
+
+#returns index of elements sotred by ratings in decreasing order.
 idx = np.argsort(my_preds, axis=0)[::-1]
 
+#prints top 10 rated movies rated by users  uses our earlier made movie array.
 print("Top 10 movie predictions:")
 for i in range(10):
     j = int(idx[i])
